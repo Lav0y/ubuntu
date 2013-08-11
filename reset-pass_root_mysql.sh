6212affd8507e0dd9d72008a777fd518cc9bd201
Trey H <treyh@ub1204server.(none)>
2013-08-10 23:57:41 -0500

    Version: 01a

--- reset-pass_root_mysql.sh
+++ reset-pass_root_mysql.sh
@@ -3,6 +3,7 @@
 # Version: 01a                                        #
 #######################################################
 
+echo "You will be prompted to put in the root MySQL password 3 times..."
 mysql -u root -p mysql -e "FLUSH PRIVILEGES;"
 mysql -u root -p mysql -e "SET PASSWORD FOR root@'localhost' = PASSWORD('');"
 mysql -u root -p mysql -e "UPDATE mysql.user SET Password=PASSWORD('') WHERE User='root';"
