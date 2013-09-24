ee7bd8aefb8276ea398fd642e3cde6dffad2a2d8
Trey Hunsucker <trey.hunsucker@gmail.com>
2013-09-24 13:11:47 -0500

    Version: 01b

---
+++ misc/update.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#######################################################
+# Version: 01b                                        #
+#######################################################
+
+# Update package lists and Install packages
+apt-get update
+apt-get dist-upgrade -y
+apt-get autoremove
+
+echo && echo "Reboot Now?"
+PS3='(Press 1 or 2): '
+options=("Yes" "No")
+select opt in "${options[@]}"
+do
+case $opt in
+	"Yes")
+		reboot now
+break
+;;
+	"No")
+		echo "...I recommend to reboot after an update!"
+break
+;;
+        *) echo invalid option;;
+esac
+done
