58d6b4cf995ceca08141585f171ff0a94d8d7d0e
Trey Hunsucker <trey.hunsucker@gmail.com>
2013-10-01 12:41:14 -0500

    Version: 01a

--- proxy/install-dansguardian.sh
+++ proxy/install-dansguardian.sh
@@ -20,6 +20,6 @@ service dansguardian start
 FINDIP=`ifconfig | grep 'inet addr:'| grep -v '127.0.0.1' | cut -d: -f2 | awk '{ print $1}'`
 echo && echo "The dansguardian install script is complete!!!" && echo
 echo "Configure your browser to use $FINDIP:8080 to begin using your new content filter." && echo
-echo "...BTW make sure that you install squid3, as dansguardian requires a proxy to work!"
+echo "....BTW make sure that you install squid3, as dansguardian requires a proxy to work!"
 
 exit 0
